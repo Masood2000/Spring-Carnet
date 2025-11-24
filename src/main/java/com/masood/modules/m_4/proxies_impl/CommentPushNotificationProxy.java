cb2448241befa9a20332a6f72d5c3e8900ed6a5b
Masood2000 <masudurrehman2000@gmail.com>
2025-11-24 14:27:35 +0100

    module-end: 4.3

--- src/main/java/com/masood/modules/m_4/proxies_impl/CommentPushNotificationProxy.java
+++ src/main/java/com/masood/modules/m_4/proxies_impl/CommentPushNotificationProxy.java
@@ -12,6 +12,8 @@ public class CommentPushNotificationProxy implements CommentNotificationProxy {
 
     @Override
     public void sendComment(Comment comment) {
+
         System.out.println("Sending Comment via PUSH Notification -> "+comment.getText());
+
     }
 }
