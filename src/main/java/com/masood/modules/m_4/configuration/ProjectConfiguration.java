07bc9aeafd2b3cff3926155a1d468b8eece0b7df
Masood2000 <masudurrehman2000@gmail.com>
2025-11-24 14:26:30 +0100

    module-end: 4.3

--- src/main/java/com/masood/modules/m_4/configuration/ProjectConfiguration.java
+++ src/main/java/com/masood/modules/m_4/configuration/ProjectConfiguration.java
@@ -1,12 +1,5 @@
 package com.masood.modules.m_4.configuration;
 
-
-import com.masood.modules.m_4.proxies.CommentNotificationProxy;
-import com.masood.modules.m_4.proxies_impl.EmailCommentNotificationProxy;
-import com.masood.modules.m_4.repositories.CommentRepository;
-import com.masood.modules.m_4.repositories_impl.DbCommentRepository;
-import com.masood.modules.m_4.services.CommentService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
