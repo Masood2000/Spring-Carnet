9e9e4a13c5a91e5ab046d2bb362740723317cea4
Masood2000 <masudurrehman2000@gmail.com>
2025-11-27 09:43:37 +0100

    module-end: 4.3

--- src/main/java/com/masood/modules/m_4/repositories/CommentRepository.java
+++ src/main/java/com/masood/modules/m_4/repositories/CommentRepository.java
@@ -3,6 +3,8 @@ package com.masood.modules.m_4.repositories;
 import com.masood.modules.m_4.entities.Comment;
 
 
+
+
 public interface CommentRepository {
 
     public void  storeComment(Comment comment);
