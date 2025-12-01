ee460179844c7fbc0ac5da15f007f8bcd5d5bbba
Masood2000 <masudurrehman2000@gmail.com>
2025-12-01 01:05:37 +0100

    module-end: 4.3

--- src/main/java/com/masood/modules/m_4/proxies_impl/CommentPushNotificationProxy.java
+++ src/main/java/com/masood/modules/m_4/proxies_impl/CommentPushNotificationProxy.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
-
 @Component
 @Qualifier("push_notification_proxy")
 public class CommentPushNotificationProxy implements CommentNotificationProxy {
