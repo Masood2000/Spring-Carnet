c44fed1eb7d1c2e23dad8a808337d2abf72bd089
Masood2000 <masudurrehman2000@gmail.com>
2025-12-27 20:05:07 +0100

    module-end: 5.2

--- src/main/java/com/masood/modules/m_4/services/CommentService.java
+++ src/main/java/com/masood/modules/m_4/services/CommentService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-
 /***
  * It's a services class and  we don't
  * need to create interface for them so annotating
