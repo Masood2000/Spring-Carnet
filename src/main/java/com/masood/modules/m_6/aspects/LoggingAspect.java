8c537f17f1a31562b177d610bc37daa5007c352a
Masood2000 <masudurrehman2000@gmail.com>
2025-12-29 19:00:12 +0100

    module-resume: 6.2

--- src/main/java/com/masood/modules/m_6/aspects/LoggingAspect.java
+++ src/main/java/com/masood/modules/m_6/aspects/LoggingAspect.java
@@ -1,11 +1,13 @@
 package com.masood.modules.m_6.aspects;
 
 
+import com.masood.modules.m_6.entities.Comment;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 @Aspect
@@ -15,11 +17,24 @@ public class LoggingAspect {
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
     @Around("execution(* com.masood.modules.m_6.services.*.*(..))")
-    public void log(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    public Object log(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
-        logger.info("Method will execute");
-        proceedingJoinPoint.proceed();
-        logger.info("Method executed");
+
+        String methodName = proceedingJoinPoint.getSignature().getName();
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        //Comment cm = (Comment) args[0];
+
+
+        logger.info("Method " + methodName + " with parameters " + Arrays.asList(args) + " will execute");
+
+        Object returnedByMethod = proceedingJoinPoint.proceed();
+
+        logger.info("Method executed and returned " + returnedByMethod);
+
+        return returnedByMethod;
+
+
 
     }
 
